a6f47f72b7245a883c2d33e991be8aefe022020e
Theodora Nutaitis-Lopez <99051640+TheodoraNell@users.noreply.github.com>
2022-09-14 13:13:43 -0700

    finalized element and text formatting

--- Dashboard/static/js/app.js
+++ Dashboard/static/js/app.js
@@ -63,9 +63,13 @@ function filterTable(elementValue, filterId) {
   //10. Finally, rebuild the table using the filtered data
   buildTable(filteredData); 
 };
+
+
   
 // 2. Attach an event to listen for changes to each filter
 d3.selectAll("input").on("change", updateFilters); 
+
+
 
 // Build the table when the page loads
 buildTable(tableData);  
